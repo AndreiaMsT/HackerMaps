610b9f8684a6928c4a5be725cc080604da9c7b74
AndreiaMsT <am.teixeira4@gmail.com>
2023-11-18 16:34:42 +0000

    save

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, {Component} from 'react';
-import Navigation from './components/Navigation';
-import './App.css';
+import React, { useState } from "react";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 const title = "HackerMaps";
-const locations = [
-    'Lombard St, San Francisco, CA, USA',
-    'PIER 39, The Embarcadero, San Francisco, CA, USA',
-    'Golden Gate Bridge, San Francisco, CA, USA',
-    `Fisherman's Wharf, San Francisco, CA, USA`,
-    'Alcatraz Island, San Francisco, CA, USA'
+const initialLocations = [
+  "Lombard St, San Francisco, CA, USA",
+  "PIER 39, The Embarcadero, San Francisco, CA, USA",
+  "Golden Gate Bridge, San Francisco, CA, USA",
+  `Fisherman's Wharf, San Francisco, CA, USA`,
+  "Alcatraz Island, San Francisco, CA, USA",
 ];
 
-class App extends Component {
+const App = () => {
+  const [locations, setLocations] = useState(initialLocations);
 
-    constructor() {
-        super();
-        this.locations = [...locations];
-    }
+  const updateLocations = (newLocations) => {
+    setLocations(newLocations);
+  };
 
-    render() {
-        return (
-            <div className="App">
-                <h8k-navbar header={title}></h8k-navbar>
-                <Navigation locations={this.locations}/>
-            </div>
-        );
-    }
-}
+  return (
+    <div className="App">
+      <h8k-navbar header={title}></h8k-navbar>
+      <Navigation locations={locations} onLocationsChange={updateLocations} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,63 +1,98 @@
-import React, {Component} from "react";
-import './Navigation.css';
+import React from "react";
+import "./Navigation.css";
 
-export default class Navigation extends Component {
-
-    // Used for rendering
-    getClasses(ctx, index) {
-        let classes = `material-icons ${ctx}`;
-        if (ctx === 'dots') {
-            if (this.isLast(index)) {
-                classes += ' hidden';
-            }
-        } else {
-            classes += this.isLast(index) ? ' small' : ' x-small';
-            if (index === 0) {
-                classes += ' first';
-            }
-        }
-        return classes;
+function Navigation({ locations, onLocationsChange }) {
+  function getClasses(ctx, index) {
+    let classes = `material-icons ${ctx}`;
+    if (ctx === "dots") {
+      if (isLast(index)) {
+        classes += " hidden";
+      }
+    } else {
+      classes += isLast(index) ? " small" : " x-small";
+      if (index === 0) {
+        classes += " first";
+      }
     }
+    return classes;
+  }
 
-    // Used for rendering
-    isLast(index) {
-        return index === this.props.locations.length - 1;
-    }
+  function isLast(index) {
+    return index === locations.length - 1;
+  }
 
-    render() {
-        return (
-            <div className="layout-row align-items-center justify-content-center navigation-screen">
-                <div className="card layout-row flat map-card">
-                    <section className="card pb-16 pr-16 flex-auto layout-column justify-content-center">
-                        <ul className="pl-0" data-testid="location-list">
-                                {/*Use this li for rendering each location item as it contains all the data-testid attributes required for the tests to pass*/}
+  function isFirst(index) {
+    return index === 0;
+  }
 
-                                {/*<li key={'row' + index} data-testid={'location-' + index}*/}
-                                {/*    className="layout-row justify-content-between align-items-center mr-8 pl-40 relative">*/}
-                                {/*    <div className="layout-column justify-content-start align-items-center handle">*/}
-                                {/*        <i className={this.getClasses('marker', index)}>{this.isLast(index) ? 'room' : 'radio_button_checked'}</i>*/}
-                                {/*        <i className={this.getClasses('dots', index)}>more_vert</i>*/}
-                                {/*    </div>*/}
-                                {/*    <div className="location-name">*/}
-                                {/*        <p className="caption text-start mb-4" data-testid="location">{location}</p>*/}
-                                {/*    </div>*/}
-                                {/*    <div>*/}
-                                {/*        <button className="icon-only small mx-0" data-testid="up-button">*/}
-                                {/*            <i className="material-icons">arrow_upward</i>*/}
-                                {/*        </button>*/}
-                                {/*        <button className="icon-only small mx-0" data-testid="down-button">*/}
-                                {/*            <i className="material-icons">arrow_downward</i>*/}
-                                {/*        </button>*/}
-                                {/*    </div>*/}
-                                {/*</li>*/}
-                        </ul>
-                    </section>
-                    <section className="flex-auto">
-                        <img src="images/map.svg" className="fill" alt="map"/>
-                    </section>
+  function moveUp(index) {
+    let allLocations = locations;
+    let currentLocation = allLocations[index];
+    allLocations[index] = allLocations[index - 1];
+    allLocations[index - 1] = currentLocation;
+    onLocationsChange(allLocations);
+  }
+
+  function moveDown(index) {
+    let allLocations = locations;
+    let currentLocation = allLocations[index];
+    allLocations[index] = allLocations[index + 1];
+    allLocations[index + 1] = currentLocation;
+    onLocationsChange(allLocations);
+  }
+
+  return (
+    <div className="layout-row align-items-center justify-content-center navigation-screen">
+      <div className="card layout-row flat map-card">
+        <section className="card pb-16 pr-16 flex-auto layout-column justify-content-center">
+          <ul className="pl-0" data-testid="location-list">
+            {locations.map((location, index) => (
+              <li
+                key={"row" + index}
+                data-testid={"location-" + index}
+                className="layout-row justify-content-between align-items-center mr-8 pl-40 relative"
+              >
+                <div className="layout-column justify-content-start align-items-center handle">
+                  <i className={getClasses("marker", index)}>
+                    {isLast(index) ? "room" : "radio_button_checked"}
+                  </i>
+                  <i className={getClasses("dots", index)}>more_vert</i>
                 </div>
-
-            </div>
-        );
-    }
+                <div className="location-name">
+                  <p className="caption text-start mb-4" data-testid="location">
+                    {location}
+                  </p>
+                </div>
+                <div>
+                  {!isFirst(index) && (
+                    <button
+                      onClick={() => moveUp(index)}
+                      className="icon-only small mx-0"
+                      data-testid="up-button"
+                    >
+                      <i className="material-icons">arrow_upward</i>
+                    </button>
+                  )}
+                  {!isLast(index) && (
+                    <button
+                      onClick={() => moveDown(index)}
+                      className="icon-only small mx-0"
+                      data-testid="down-button"
+                    >
+                      <i className="material-icons">arrow_downward</i>
+                    </button>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section className="flex-auto">
+          <img src="images/map.svg" className="fill" alt="map" />
+        </section>
+      </div>
+    </div>
+  );
 }
+
+export default Navigation;
